577ffda2c8d7a639e095d28f9b860af8c515e630
o-ishchenko <olya.galaychuk@gmail.com>
2021-11-27 13:46:40 +0200

    Добавляет решение 1 задачи

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtnEl = document.querySelector('[data-start]');
+const stopBtnEl = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let intervalId = null;
 
+startBtnEl.addEventListener('click', onStartButtonClick);
+stopBtnEl.addEventListener('click', onStopButtonClick);
+
+function onStartButtonClick() {
+    intervalId = setInterval(changeColor, 1000);
+    startBtnEl.setAttribute('disabled', true);
+    stopBtnEl.removeAttribute('disabled');
+};
+
+function onStopButtonClick() {
+    clearInterval(intervalId);
+    startBtnEl.removeAttribute('disabled');
+    stopBtnEl.setAttribute('disabled', true);
+    };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
+
+function changeColor() {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+}
