860ead20febf131d5aa8b44722a088a4a76fa6f9
o-ishchenko <olya.galaychuk@gmail.com>
2021-11-29 16:00:56 +0200

    добавялет 2 и 3 задание и стили

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+import Notiflix from 'notiflix';
+const formEl = document.querySelector('.form');
+const buttonEl = document.querySelector('button');
+formEl.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+     setTimeout(() => {
+      if (shouldResolve) {
+        resolve ({ position, delay });
+      } else {
+        reject ({ position, delay });
+      }
+    }, delay)
+  });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  const formElements = event.currentTarget.elements;
+  const delayOnStart = Number(formElements.delay.value);
+  const step = Number(formElements.step.value);
+  const amount = Number(formElements.amount.value);
+  let delayOnStep = 0;
+  for (let i = 0; i < amount; i += 1) {
+    delayOnStep = delayOnStart + i*step;
+    createPromise(i+1, delayOnStep)
+      .then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+      })
+      .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
   }
+ 
 }
